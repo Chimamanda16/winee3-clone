9dab1c1ccd352e9a76e5f6a26cf4951420bb087f
Chimamanda16 <chimamandaunachukwu@gmail.com>
2024-09-27 13:57:13 +0100

    Add media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,15 @@ const Navbar = ()=>{
     <div className="nav">
         <img src="../images/logo.png" alt="winee3 logo" className="logo"/>
         <div className="nav-btn">
-            <NavBtn url="../images/coingecko.webp" alt="coingecko image" text="CoinGecko"/>
-            <NavBtn url="../images/gate.io.webp" alt="gate.io image" text="Gate.io"/>
-            <NavBtn text="Contracts"/>
+            <div className="hamburger">
+                <i className="fa-solid fa-bars burger-bar"></i>
+            </div>
+            <div className="nav-items">
+
+                <NavBtn url="../images/coingecko.webp" alt="coingecko image" text="CoinGecko"/>
+                <NavBtn url="../images/gate.io.webp" alt="gate.io image" text="Gate.io"/>
+                <NavBtn text="Contracts"/>
+                </div>
         </div>
     </div>
     </>
