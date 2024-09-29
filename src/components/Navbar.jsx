e0e85e02a53505d1640a1fe9c9d496bc6231cc63
Chimamanda16 <chimamandaunachukwu@gmail.com>
2024-09-29 15:23:39 +0100

    Add navbar media query

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,39 @@
 import "./CSS/Navbar.css"
 import NavBtn from "./IntroBtn.jsx";
+import { useState, useRef } from "react";
 
 const Navbar = ()=>{
+    const [navOpen, setNavOpen] = useState(false);
+    const navRef = useRef(null);
+    const navItemsRef =useRef(null);
+    function handleClick(){
+        setNavOpen(!navOpen);
+        if(navOpen === true){
+            navRef.current.classList.add("nav-open")
+            navItemsRef.current.classList.add("nav-items-open");
+        }
+        else{
+            navRef.current.classList.remove("nav-open");
+            navItemsRef.current.classList.remove("nav-items-open");
+        }
+    }
     return <>
-    <div className="nav">
-        <img src="../images/logo.png" alt="winee3 logo" className="logo"/>
-        <div className="nav-btn">
-            <div className="hamburger">
-                <i className="fa-solid fa-bars burger-bar"></i>
-            </div>
-            <div className="nav-items">
-
-                <NavBtn url="../images/coingecko.webp" alt="coingecko image" text="CoinGecko"/>
-                <NavBtn url="../images/gate.io.webp" alt="gate.io image" text="Gate.io"/>
-                <NavBtn text="Contracts"/>
+        <div className="nav" ref= {navRef} >
+            <div className="nav-header">
+                <img src="../images/logo.png" alt="winee3 logo" className="logo"/>
+                <div className="hamburger" onClick={handleClick}>
+                    <i className="fa-solid fa-bars burger-bar"></i>
                 </div>
+            </div>
+            <div className="nav-btn">
+                <div className="nav-items" ref={navItemsRef}>
+
+                    <NavBtn url="../images/coingecko.webp" alt="coingecko image" text="CoinGecko"/>
+                    <NavBtn url="../images/gate.io.webp" alt="gate.io image" text="Gate.io"/>
+                    <NavBtn text="Contracts"/>
+                    </div>
+            </div>
         </div>
-    </div>
     </>
 }
 
